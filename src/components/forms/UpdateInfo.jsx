1fa5f2ce90b779e7760c30fced379a37420ef009
StartOff123 <startoff123@mail.ru>
2023-04-26 18:15:45 +0500

    new server URL

--- src/components/forms/UpdateInfo.jsx
+++ src/components/forms/UpdateInfo.jsx
@@ -60,7 +60,7 @@ const UpdateInfo = ({ onClose }) => {
                         <div className='form-avatar'>
                             <label>
                                 {imageUrl ?
-                                    <div style={{ background: `url(http://localhost:5555${imageUrl})`, backgroundPosition: 'center', backgroundSize: 'cover', objectFit: 'cover', height: '100%' }}></div> :
+                                    <div style={{ background: `url(https://codekids-backend-production.up.railway.app${imageUrl})`, backgroundPosition: 'center', backgroundSize: 'cover', objectFit: 'cover', height: '100%' }}></div> :
                                     <svg xmlns="http://www.w3.org/2000/svg" width="70" height="70" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
                                         <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
                                         <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
